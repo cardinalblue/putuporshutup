ced568237d7af8ca1c1c1970f97d14dfd4750f72
jennyshih <jinghua.shih@gmail.com>
2019-05-02 23:09:37 +0800

    added bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  get 'itineraries/index'
+
+  get 'itineraries/new'
+
+  get 'itineraries/create'
+
+  get 'itineraries/edit'
+
+  get 'itineraries/update'
+
+  get 'itineraries/show'
+
+  get 'itineraries/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
