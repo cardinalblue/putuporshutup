5333778fa8035d3c14057d6f3b4d30bfcf973aa7
jennyshih <jinghua.shih@gmail.com>
2019-05-02 23:34:36 +0800

    added events controller and itinerary controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,6 @@
 Rails.application.routes.draw do
-  get 'itineraries/index'
+  root 'itineraries#index'
 
-  get 'itineraries/new'
-
-  get 'itineraries/create'
-
-  get 'itineraries/edit'
-
-  get 'itineraries/update'
-
-  get 'itineraries/show'
-
-  get 'itineraries/destroy'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :itineraries
+  resources :events
 end
