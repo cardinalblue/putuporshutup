5d378d0f90d77a9c93bec814945462de0fb3fea1
jennyshih <jinghua.shih@gmail.com>
2019-05-03 16:49:35 +0800

    updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,10 @@
 users = [
-  { name: 'Grace', account: 'grace@hack', payment_account: 'grace' },
-  { name: 'Jenny', account: 'jenny@hack', payment_account: 'jenny' },
-  { name: 'Amelia', account: 'amelia@hack', payment_account: 'amelia' },
-  { name: 'Minnie', account: 'minnie@hack', payment_account: 'minnie' },
-  { name: 'Random', account: 'random@hack', payment_account: 'random' },
-  { name: 'Russell', account: 'russell@hack', payment_account: 'russell' }
+  { name: 'Grace', account: 'grace@hack', email: 'grace@hack', password:'123456', payment_account: 'grace' },
+  { name: 'Jenny', account: 'jenny@hack', email: 'jenny@hack', password:'123456', payment_account: 'jenny' },
+  # { name: 'Amelia', account: 'amelia@hack', password:'123456', payment_account: 'amelia' },
+  # { name: 'Minnie', account: 'minnie@hack', password:'123456', payment_account: 'minnie' },
+  # { name: 'Random', account: 'random@hack', password:'123456', payment_account: 'random' },
+  # { name: 'Russell', account: 'russell@hack', password:'123456', payment_account: 'russell' }
 ]
 
 # Create users
@@ -13,8 +13,8 @@ users.each do |u|
 end
 
 # Create itineraries
-1.upto(6).each do |num|
-  Itinerary.create(title: "hacking ##{num}", organizer_id: num)
+1.upto(3).each do |num|
+  Itinerary.create(title: "hacking ##{num}", organizer_id: num % 2 + 1)
 
   1.upto(5).each do |i|
     Event.create(title: "fun stuff ##{i}",
@@ -32,7 +32,7 @@ end
 
 # Create reservations
 1.upto(20).each do |i|
-  Reservation.create(event_id: i % 5 + 1,
-                     attendee_id: i % 6 + 1,
+  Reservation.create(event_id: i % 3 + 1,
+                     attendee_id: i % 2 + 1,
                      status: i % 3)
 end
