1df80e6c20414eaf703e46c13f94508f77cac2ad
jennyshih <jinghua.shih@gmail.com>
2019-05-03 08:39:06 +0800

    added flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def flash_messages(opts = {})
+    flash.each do |msg_type, msg|
+      concat(
+        content_tag(:div, msg, class: "alert alert-#{msg_type} alert-dismissable fade-in") do
+          concat content_tag(:button, 'x', class: "close", data: { dismiss: 'alert' })
+          concat msg.html_safe
+        end
+      )
+    end
+    nil
+  end
 end
